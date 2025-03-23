91a9a1573b8e39dbb0c9b1cdf164a6d306fef8a2
xie186 <xieshaojun0526@gmail.com>
2025-03-23 14:54:50 +0000

    Add generate_volcano_plot

--- volcanim/__init__.py
+++ volcanim/__init__.py
@@ -1,6 +1,6 @@
 from .cli import main
 from .highlight import draw_gene_dots
 from .legend import create_legend
-from .plot import plot_w_PMobject_batch, 
-                  EnhancedVolcano, 
+from .plot import plot_w_PMobject_batch, \
+                  EnhancedVolcano,       \
                   generate_volcano_plot
